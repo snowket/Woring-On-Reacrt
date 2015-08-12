47e808497f2d6613632b3a612585ed0b34fb8462
Lasha <lasha.janjgava@hotmail.com>
2015-08-13 01:55:48 +0400

    push

---
+++ vue.js
@@ -0,0 +1,8 @@
+var master = React.createClass({
+	render: function() {
+		return (
+			<div className="master"><h2>get in </h2></div>
+		);
+	}
+});
+React.render(<master ,document.getElementById('example'));
